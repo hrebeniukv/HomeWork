7a7762056ff4900851ab35fa6ecea869365359eb
Volodymyr Hrebeniuk <hrebeniukv@gmail.com>
2022-09-18 21:01:08 +0200

    tets

---
+++ HomeWork1/HW1.py
@@ -0,0 +1,49 @@
+# Задача: Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії (+, -, *, / и тд.) для цих чисел.
+
+first = 10
+second = 30
+
+addition_res = first + second
+print('addition -->', addition_res)
+
+subtraction_res = first - second
+print('subtraction -->', subtraction_res)
+
+multiplication_res = first * second
+print('multiplication -->', multiplication_res)
+
+division_res = first * second
+print('division -->', division_res)
+
+modulus_res = second % first
+print('modulus -->', modulus_res)
+
+floor_devision_res = second // first
+print('floor_devision -->', floor_devision_res)
+
+exponentiation_res = second ** first
+print('exponentiation -->', exponentiation_res)
+
+
+# Задача: Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 1. Виведіть на екран результат кожного порівняння.
+
+variable_comparing = first > second
+print('first > second -->', variable_comparing)
+
+variable_comparing = first < second
+print('first < second -->', variable_comparing)
+
+variable_comparing = first >= second
+print('first >= second -->', variable_comparing)
+
+variable_comparing = first <= second
+print('first <= second -->', variable_comparing)
+
+variable_comparing = first == second
+print('first == second -->', variable_comparing)
+
+variable_comparing = first != second
+print('first != second -->', variable_comparing)
+
+
+
