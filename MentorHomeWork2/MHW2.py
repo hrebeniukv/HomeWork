6207dc416d53b1c0b78a34249ec54898edc7bdb1
Volodymyr Hrebeniuk <hrebeniukv@gmail.com>
2022-10-16 20:19:23 +0200

    Test data

---
+++ MentorHomeWork2/MHW2.py
@@ -0,0 +1,70 @@
+# #
+def print_result(print_template):
+    def dec(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print(f"{print_template} is: {result} ")
+            return result
+
+        return wrapper
+
+    return dec
+
+
+# 1.напишіть функцію, яка перевірить, чи передане їй число є парним чи непарним (повертає True False)
+
+
+@print_result('Result of checking the number to parity')
+def cheking_nubmer(test_number) -> bool:
+    '''
+    This function check if number is parity. https://en.wikipedia.org/wiki/Parity_(mathematics)
+    :param test_number: mandatory argument which should be number
+    :return: Return True if number is parity or False if number isn't parity
+    '''
+    try:
+        return True if float(test_number) % 2 == 0 else False
+    except:
+        return False
+
+
+assert cheking_nubmer(10) is True
+assert cheking_nubmer(9999999) is False
+assert cheking_nubmer('10.00') is True
+assert cheking_nubmer('not a number') is False
+assert cheking_nubmer(10.2) is False
+assert cheking_nubmer(-10) is True
+assert cheking_nubmer(0) is True
+assert type(cheking_nubmer(10)) is bool
+
+
+# cheking_nubmer(input("Please, enter the number: "))
+
+
+# 2. напишіть функцію. яка приймає стрічку, і визначає, чи дана стрічка відповідає результатам роботи методу capitalize()
+# (перший символ є верхнім регістром, а решта – нижнім.) (повертає True False)
+
+@print_result('Result of checking the string')
+def checking_string(test_string: str) -> bool:
+    '''
+    This function check if the text is in correct format.
+    :param test_string: mandatory position argument
+    :return: function return the "True" if text match to capitalize format or False if text doesn't match.
+    '''
+    try:
+        test_string = str(test_string)
+        return True if test_string[0].isalpha() and test_string == test_string.capitalize() else False
+    except:
+        print('Something was wrong')
+
+
+# checking_string(input("Please, enter some text: "))
+
+
+assert type(checking_string('Some text')) is bool
+assert checking_string(10) == False
+assert checking_string('test') == False
+assert checking_string('Test') == True
+assert checking_string('Test Test') == False
+assert checking_string('TEST ') == False
+assert checking_string('!Test') == False
+assert checking_string('Інша мова') == True
---
+++ test_data.py
@@ -0,0 +1,118 @@
+# # #
+# def print_result(print_template):
+#     def dec(func):
+#         def wrapper(*args, **kwargs):
+#             result = func(*args, **kwargs)
+#             print(f"{print_template} is: {result} ")
+#             return result
+#
+#         return wrapper
+#
+#     return dec
+#
+#
+# #
+# #
+# # #
+# # def checking_on_number():
+# #     def wrapper(*args, **kwargs):
+# #
+# #         try:
+# #             for i in args:
+# #                 float(i)
+# #
+# #             for y in kwargs.values():
+# #                 float(y)
+# #             return map(float, (args)), map(float, (kwargs))
+# #         except ValueError:
+# #             print("Entered data is not number")
+# #             return "data is not a number"
+# #
+# #     return wrapper
+# #
+# #
+# # # # 1.напишіть функцію, яка перевірить, чи передане їй число є парним чи непарним (повертає True False)
+# # #
+# # @print_result('Result of checking the number')
+# # # @checking_on_number
+# # def cheking_nubmer(test_number) -> bool:
+# #     result = True if float(test_number) % 2 == 0 else False
+# #     return result
+# #
+# #
+# # cheking_nubmer(input("Please, enter the number: "))
+# #
+# #
+# # #
+# # #
+# # # assert type(cheking_nubmer(2)) is bool
+# # # # assert
+# # # # assert
+# # # # assert
+# # # # assert
+# # #
+# # #
+# # # # 2. напишіть функцію. яка приймає стрічку, і визначає, чи дана стрічка відповідає результатам роботи методу capitalize()
+# # # # (перший символ є верхнім регістром, а решта – нижнім.) (повертає True False)
+# # #
+# # # # @print_result('Result of checking the string')
+# # # # def checking_string(test_string: str) -> bool:
+# # # #     return True if test_string == test_string.capitalize() else False
+# # # #
+# # # #
+# # # # checking_string(input("Please, enter some text: "))
+# # #
+# # #
+# # # # # assert
+# # # # # assert
+# # # # # assert
+# # # # # assert
+# # # # # assert
+# # #
+# # #
+# # #
+# # #
+# # #
+# #
+# # def changing_to_number(func):
+# #     def wrapper(string_number):
+# #
+# #         try:
+# #             test_number = float(string_number)
+# #             return func(test_number)
+# #         except ValueError:
+# #             # print("Entered data is not number")
+# #             return "data is not a number"
+# #
+# #     return wrapper
+#
+# @print_result('Result of checking the number to parity')
+# def cheking_nubmer(test_number) -> bool:
+#     try:
+#         return True if float(test_number) % 2 == 0 else False
+#     except:
+#         return False
+#
+#
+#
+#
+#
+#
+# assert cheking_nubmer(10) is True
+# assert cheking_nubmer(9999999) is False
+# assert cheking_nubmer('10.00') is True
+# assert cheking_nubmer('not a number') is False
+# assert cheking_nubmer(10.2) is False
+# assert cheking_nubmer(-10) is True
+# assert cheking_nubmer(0) is True
+# assert type(cheking_nubmer(10)) is bool
+
+
+# print('10'.capitalize())
+
+
+def d(test_string='!Ttttt'):
+    return True if test_string[0].isalpha() and test_string == test_string.capitalize() else False
+
+
+print(d())
